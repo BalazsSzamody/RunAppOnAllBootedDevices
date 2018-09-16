b184fabaa1fe65060b88a70ddd4f25770d8cb425
BalazsSzamody <bszamody@gmail.com>
2018-09-16 20:19:56 +1000

    Initial Commit

---
+++ RunAppOnAllBootedDevices/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RunAppOnAllBootedDevices
+//
+//  Created by Balazs Szamody on 16/9/18.
+//  Copyright © 2018 Balazs Szamody. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
